2d0cdbe496f6b7b55ef6f478ee8f6bf8d8c6f105
santu-sarker <mdsantusarker@gmail.com>
2023-01-09 14:46:22 +0600

    complete version

--- js/draft_version.js
+++ js/draft_version.js
@@ -1,4 +1,3 @@
-// import { create_row } from "./user_operations.js";
 const user = [
   {
     _id: 1,
@@ -160,9 +159,48 @@ const user = [
     phone: "+1 (976) 493-2195",
     address: "903 Newel Street, Clarence, Pennsylvania, 4033",
   },
-].reverse();
+];
 
-//  ***************** table row creation function  ******************
+// **************  html elements and global variable declaration area *****************
+
+// ************ html entities  ************
+const table_body = document.getElementById("table_body");
+const modal = new bootstrap.Modal("#user_modal");
+const edit_modal = new bootstrap.Modal("#edit_modal");
+const delete_modal = new bootstrap.Modal("#delete_modal");
+
+// edit modal html entities
+const edit_id = document.getElementById("edit_id");
+const edit_name = document.getElementById("edit_name");
+const edit_email = document.getElementById("edit_email");
+const edit_phone = document.getElementById("edit_phone");
+const edit_company = document.getElementById("edit_company");
+const edit_address = document.getElementById("edit_address");
+const edit_gender = document.getElementsByName("edit_gender");
+const edit_submit = document.getElementById("edit_submit"); // save edited data button
+
+// table buttons
+
+let edit_buttons = document.querySelectorAll(".edit_user");
+// ************** variables **********
+
+// user data variables
+const add_user = document.getElementById("user_submit");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const company = document.getElementById("company");
+const address = document.getElementById("address");
+const gender = document.getElementsByName("gender");
+
+// ******** paginate variables
+const per_page = 5;
+let total_page = Math.ceil(user.length / per_page);
+let current_position = 0;
+let current_page = 1;
+
+// ********************* functions section **************
+//  ******************* row creating main function ***************
 
 const create_row = (item) => {
   let row = document.createElement("tr");
@@ -179,6 +217,7 @@ const create_row = (item) => {
   let delete_btn = document.createElement("button");
 
   id.innerText = item._id;
+  // console.log(item);
   name.innerText = item.name;
   gender.innerText = item.gender;
   company.innerText = item.company;
@@ -209,7 +248,7 @@ const create_row = (item) => {
   return row;
 };
 
-// pagination creating function
+// ************** creating pagination  function ****************
 const create_pagination = (total_page) => {
   const paginate_elements = [];
 
@@ -218,7 +257,7 @@ const create_pagination = (total_page) => {
   let nxt_li = document.createElement("li");
   let prev_btn = document.createElement("button");
   let nxt_btn = document.createElement("button");
-  prev_li.classList.add("page-item", "paginate_prev", "disabled");
+  prev_li.classList.add("page-item", "paginate_prev");
   prev_btn.classList.add("page-link");
   prev_btn.setAttribute("id", "prev_btn");
   prev_btn.innerText = "previous";
@@ -242,30 +281,70 @@ const create_pagination = (total_page) => {
   nxt_btn.innerText = "next";
   nxt_li.appendChild(nxt_btn);
   paginate_elements.push(nxt_li);
-  return paginate_elements;
+
+  // paginate parent html entities
+  const paginate_parent = document.getElementById("paginate_parent");
+  paginate_parent.innerHTML = "";
+  //   paginate_parent.append(paginate_elements);
+  //   const elements = create_pagination(total_page);
+  paginate_elements.forEach((item) => {
+    paginate_parent.appendChild(item);
+  });
+  //   console.log(paginate_elements);
 };
 
-//  updated load_user
 const load_user = (page) => {
+  // inserting rows one by one
   table_body.innerHTML = ""; // removing all child node of table
   current_position = page * per_page - per_page;
 
   let user_data = user.slice(current_position, current_position + per_page);
-
-  // inserting rows one by one into table body
+  //console.log(user_data);
   user_data.forEach((item) => {
     let row = create_row(item);
     table_body.appendChild(row);
   });
 
-  // *********** edit reset section **************
+  //   resetting pagination
+  total_page = Math.ceil(user.length / per_page);
+  // console.log(total_page);
+  create_pagination(total_page);
+  let paginate_buttons = document.querySelectorAll(".paginate_btn");
+  paginate_buttons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      let btn_parent = btn.parentElement.parentElement.children;
+      console.log(btn_parent);
+      for (let i = 1; i <= btn_parent.length - 2; i++) {
+        //looping through each li and remove active class
+        if (btn_parent[i].classList.contains("active")) {
+          btn_parent[i].classList.remove("active");
+        }
+        btn.parentElement.classList.add("active"); //adding active class  to its parent li
 
+        //  if(btn_parent[i] != 0 )
+      }
+      // event.target.classList.add("active");
+      current_page = Number(event.target.value);
+      load_user(current_page);
+      let pre_btn = document.getElementById("prev_btn");
+      let nxt_btn = document.getElementById("next_btn");
+      current_page == 1
+        ? pre_btn.parentElement.classList.add("disabled")
+        : pre_btn.parentElement.classList.remove("disabled");
+      current_page == total_page
+        ? nxt_btn.parentElement.classList.add("disabled")
+        : nxt_btn.parentElement.classList.remove("disabled");
+    });
+  });
+
+  //   resetting edit field
   edit_buttons = document.querySelectorAll(".edit_user");
 
   edit_buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
       let id = event.target.value;
       let user_data = user[id - 1];
+      // console.log(user_data);
       edit_modal.show();
       edit_id.value = user_data._id;
       edit_name.value = user_data.name;
@@ -278,10 +357,9 @@ const load_user = (page) => {
         : (edit_gender[1].checked = true);
     });
   });
+  //   resetting delete field
 
-  // delele reset section
   let delete_buttons = document.querySelectorAll(".delete_user");
-
   delete_buttons.forEach((button) => {
     button.addEventListener("click", (event) => {
       let delete_id = document.getElementById("delete_id");
@@ -291,41 +369,26 @@ const load_user = (page) => {
   });
 };
 
-//  **************** fetching all user information into table  **************
-
-const table_body = document.getElementById("table_body");
-const modal = new bootstrap.Modal("#user_modal");
-const edit_modal = new bootstrap.Modal("#edit_modal");
-let edit_buttons = document.querySelectorAll(".edit_user");
-
 // adding pagination to the page
-const per_page = 5;
-const total_page = Math.ceil(user.length / per_page);
-let current_position = 0;
-let current_page = 1;
-const paginate_parent = document.getElementById("paginate_parent");
-paginate_parent.append;
-const elements = create_pagination(total_page);
-elements.forEach((item) => {
-  paginate_parent.appendChild(item);
-});
 
+create_pagination(total_page);
 load_user(current_page);
+// *************** pagination button click event *************
 
-// pagination button click event
-let paginate_buttons = document.querySelectorAll(".paginate_btn");
+let paginate_buttons = document.querySelectorAll(".paginate_btn"); // selecting all paginate button except prev and next
 paginate_buttons.forEach((btn) => {
   btn.addEventListener("click", (event) => {
     let btn_parent = btn.parentElement.parentElement.children;
-
     for (let i = 1; i <= btn_parent.length - 2; i++) {
       //looping through each li and remove active class
       if (btn_parent[i].classList.contains("active")) {
         btn_parent[i].classList.remove("active");
       }
       btn.parentElement.classList.add("active"); //adding active class  to its parent li
-    }
 
+      //  if(btn_parent[i] != 0 )
+    }
+    // event.target.classList.add("active");
     current_page = Number(event.target.value);
     load_user(current_page);
     let pre_btn = document.getElementById("prev_btn");
@@ -340,10 +403,10 @@ paginate_buttons.forEach((btn) => {
 });
 
 //  previous page button event
-const prev_btn = document.getElementById("prev_btn");
-
+// console.log(prev_btn);
+const prev_btn = document.getElementById("prev_btn"); //pagination previous button
 prev_btn.addEventListener("click", (event) => {
-  let childens = document.querySelectorAll(".paginate_btn");
+  let childens = document.querySelectorAll(".paginate_btn"); //pagination page number button
   next_btn.parentElement.classList.remove("disabled");
   if (current_page == 1) {
     prev_btn.parentElement.classList.add("disabled");
@@ -363,9 +426,9 @@ prev_btn.addEventListener("click", (event) => {
 });
 
 // next page button event
-const next_btn = document.getElementById("next_btn");
+const next_btn = document.getElementById("next_btn"); // pagination next button
 next_btn.addEventListener("click", (event) => {
-  let childens = document.querySelectorAll(".paginate_btn");
+  let childens = document.querySelectorAll(".paginate_btn"); //pagination page number button
   prev_btn.parentElement.classList.remove("disabled");
   if (current_page == 4) {
     next_btn.parentElement.classList.add("disabled");
@@ -385,26 +448,7 @@ next_btn.addEventListener("click", (event) => {
   }
 });
 
-// ************ html entites for edit modal **************
-
-const edit_id = document.getElementById("edit_id");
-const edit_name = document.getElementById("edit_name");
-const edit_email = document.getElementById("edit_email");
-const edit_phone = document.getElementById("edit_phone");
-const edit_company = document.getElementById("edit_company");
-const edit_address = document.getElementById("edit_address");
-const edit_gender = document.getElementsByName("edit_gender");
-
-// *********** html entites for add user modal  ***********
-let add_user = document.getElementById("user_submit");
-let name = document.getElementById("name");
-let email = document.getElementById("email");
-let phone = document.getElementById("phone");
-let company = document.getElementById("company");
-let address = document.getElementById("address");
-let gender = document.getElementsByName("gender");
-
-// ************* onclick event for user creation *************
+//  onclick event for user creation
 add_user.addEventListener("click", () => {
   const id = user[user.length - 1]._id + 1; // calculating last indexed user id +1
 
@@ -426,26 +470,19 @@ add_user.addEventListener("click", () => {
   };
 
   user.push(data);
-  load_user(current_page);
-
-  //clearing add user form info
-  name.value = "";
-  email.value = "";
-  phone.value = "";
-  company.value = "";
-  address.value = "";
-  gender[0].checked = false;
-  gender[1].checked = false;
+  const row = create_row(data);
+  table_body.prepend(row);
+  create_pagination();
   modal.hide();
 });
 
-// ********  editing user data buuton event *******
+//  editing user data
 
-const edit_submit = document.getElementById("edit_submit");
 edit_buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
     let id = event.target.value;
     let user_data = user[id - 1];
+    // console.log(user_data);
     edit_modal.show();
     edit_id.value = user_data._id;
     edit_name.value = user_data.name;
@@ -458,36 +495,30 @@ edit_buttons.forEach((button) => {
       : (edit_gender[1].checked = true);
   });
 });
-
-//  ************* onclick event for user edit save button *********
-
-edit_submit.addEventListener("click", (event) => {
-  let id = edit_id.value;
+//  onclick event for user edit
+edit_submit.addEventListener("click", () => {
+  let id = edit_id.value - 1;
+  edit_modal.show();
   const gen_value = edit_gender[0].checked
     ? "male"
     : edit_gender[1].checked
     ? "female"
     : "undefined";
 
-  let index = user.map((item) => item._id).indexOf(Number(id)); //checking user index
-  if (index > -1) {
-    user[index]._id = index + 1;
-    user[index].name = edit_name.value;
-    user[index].phone = edit_phone.value;
-    user[index].email = edit_email.value;
-    user[index].company = edit_company.value;
-    user[index].address = edit_address.value;
-    user[index].gender = gen_value;
-  }
+  const editable_cell = table_body.children.item(id).children;
+  editable_cell[1].innerText = edit_name.value;
+  editable_cell[2].innerText = edit_phone.value;
+  editable_cell[3].innerText = edit_email.value;
+  editable_cell[4].innerText = edit_company.value;
+  editable_cell[5].innerText = edit_address.value;
+  editable_cell[6].innerText = gen_value;
 
   edit_modal.hide();
-  load_user(current_page);
 });
 
 // ******************* Deleting user data  ******************
 let delete_buttons = document.querySelectorAll(".delete_user");
 let delete_confirm = document.getElementById("delete_confirm");
-const delete_modal = new bootstrap.Modal("#delete_modal");
 
 delete_buttons.forEach((button) => {
   button.addEventListener("click", (event) => {
@@ -498,12 +529,8 @@ delete_buttons.forEach((button) => {
 });
 delete_confirm.addEventListener("click", (event) => {
   let id = delete_id.value;
-
-  let index = user.map((item) => item._id).indexOf(Number(id)); //checking user index
-  if (index > -1) {
-    // only splice array when item is found
-    user.splice(index, 1); // 2nd parameter means remove one item only
-  }
+  user.splice(id - 1, 1); // as index start form 0
+  console.log(user);
   load_user(current_page);
   delete_modal.hide();
 });
